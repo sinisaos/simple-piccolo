b82c322826f345310ca168737f3cd9f814355d6b
sinisaos <sinisaos@gmail.com>
2023-07-06 13:22:17 +0200

    remove PiccoloORMPlugin and use create_pydantic_model

--- litestar_app.py
+++ litestar_app.py
@@ -1,14 +1,15 @@
 import asyncio
+import datetime
 import typing as t
 
 import uvicorn
 from litestar import Litestar, asgi, delete, get, patch, post
-from litestar.contrib.piccolo_orm import PiccoloORMPlugin
 from litestar.exceptions import NotFoundException
 from litestar.types import Receive, Scope, Send
 from piccolo.apps.user.tables import BaseUser
-from piccolo.columns import Boolean, Varchar
-from piccolo.table import Table
+from piccolo.columns import Boolean, ForeignKey, Interval, Varchar
+from piccolo.table import Table, create_db_tables
+from piccolo.utils.pydantic import create_pydantic_model
 from piccolo_admin.endpoints import create_admin
 from piccolo_api.session_auth.tables import SessionsBase
 
@@ -18,6 +19,20 @@ from piccolo_conf import DB
 class Task(Table, db=DB):
     name = Varchar()
     completed = Boolean(default=False)
+    duration = Interval()
+    task_user = ForeignKey(references=BaseUser)
+
+
+TaskModelIn: t.Any = create_pydantic_model(
+    table=Task,
+    model_name="TaskModelIn",
+)
+TaskModelOut: t.Any = create_pydantic_model(
+    table=Task,
+    include_default_columns=True,
+    model_name="TaskModelOut",
+    nested=True,
+)
 
 
 # mounting Piccolo Admin
@@ -27,29 +42,36 @@ async def admin(scope: "Scope", receive: "Receive", send: "Send") -> None:
 
 
 @get("/tasks", tags=["Task"])
-async def tasks() -> t.List[Task]:
-    tasks = await Task.select().order_by(Task.id, ascending=False)
+async def tasks() -> t.List[TaskModelOut]:
+    tasks = (
+        await Task.select(
+            Task.all_columns(),
+            Task.task_user.id,
+            Task.task_user.username,
+        )
+        .order_by(Task.id, ascending=False)
+        .output(nested=True)
+    )
     return tasks
 
 
 @post("/tasks", tags=["Task"])
-async def create_task(data: Task) -> Task:
-    task = Task(**data.to_dict())
+async def create_task(data: TaskModelIn) -> TaskModelOut:
+    task = Task(**data.dict())
     await task.save()
-    return task
+    return task.to_dict()
 
 
 @patch("/tasks/{task_id:int}", tags=["Task"])
-async def update_task(task_id: int, data: Task) -> Task:
+async def update_task(task_id: int, data: TaskModelIn) -> TaskModelOut:
     task = await Task.objects().get(Task.id == task_id)
     if not task:
         raise NotFoundException("Task does not exist")
-    for key, value in data.to_dict().items():
-        task.id = task_id
+    for key, value in data.dict().items():
         setattr(task, key, value)
 
     await task.save()
-    return task
+    return task.to_dict()
 
 
 @delete("/tasks/{task_id:int}", tags=["Task"])
@@ -62,9 +84,12 @@ async def delete_task(task_id: int) -> None:
 
 async def main():
     # Tables creating
-    await BaseUser.create_table(if_not_exists=True)
-    await SessionsBase.create_table(if_not_exists=True)
-    await Task.create_table(if_not_exists=True)
+    await create_db_tables(
+        BaseUser,
+        SessionsBase,
+        Task,
+        if_not_exists=True,
+    )
 
     # Creating admin users
     if not await BaseUser.exists().where(BaseUser.email == "admin@test.com"):
@@ -79,6 +104,12 @@ async def main():
         await user.save()
 
 
+# is used to serialize the Interval() column because
+# datetime.timedelta is currently not supported in
+# msgspec https://github.com/jcrist/msgspec/issues/260
+TYPE_ENCODERS = {datetime.timedelta: str}
+
+
 app = Litestar(
     route_handlers=[
         admin,
@@ -87,7 +118,7 @@ app = Litestar(
         update_task,
         delete_task,
     ],
-    plugins=[PiccoloORMPlugin()],
+    type_encoders=TYPE_ENCODERS,
 )
 
 if __name__ == "__main__":
